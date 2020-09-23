125e0eafb2b905ae5f2bda41339a84718a7c61ea
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-09-23 11:44:39 -0400

    change signin signup mutation

--- server/src/type/graph.d.ts
+++ server/src/type/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type FacebookConnectResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  FacebookConnect(name: String!, email: String, fbId: String!): FacebookConnectResponse!\n  SignIn(email: String!, password: String!): SignInResponse!\n  SignUp(email: String!, name: String!, country: String!, age: Int!, password: String!): SignUpResponse!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  name: String!\n  fbId: String\n  password: String\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignInResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype SignUpResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype getUserAllResponse {\n  users: [User]\n}\n\ntype Query {\n  GetAllUser: getUserAllResponse!\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  verified: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
+export const typeDefs = ["type FacebookConnectResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  FacebookConnect(name: String!, email: String, fbId: String!): FacebookConnectResponse!\n  SignIn(email: String!, password: String!): SignInResponse!\n  SignUp(email: String!, name: String!, country: String, age: Int, password: String!): SignUpResponse!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  name: String!\n  fbId: String\n  password: String\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignInResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype SignUpResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype getUserAllResponse {\n  users: [User]\n}\n\ntype Query {\n  GetAllUser: getUserAllResponse!\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  verified: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -43,8 +43,8 @@ export interface SignInMutationArgs {
 export interface SignUpMutationArgs {
   email: string;
   name: string;
-  country: string;
-  age: number;
+  country: string | null;
+  age: number | null;
   password: string;
 }
 
