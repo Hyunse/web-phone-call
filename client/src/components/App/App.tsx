306fd2b3919fd19e48b715909303d8111b0c769c
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-09-23 11:45:11 -0400

    Remove styled-components & Add tailwindcss

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { IS_LOGGED_IN } from './AppQueries';
-import { ThemeProvider } from 'styled-components';
-import { myTheme } from '../../theme';
-import { useQuery } from 'react-apollo-hooks'
+import { useQuery } from 'react-apollo-hooks';
 
 const App = () => {
   const { data } = useQuery(IS_LOGGED_IN);
 
   return (
     <React.Fragment>
-      <ThemeProvider theme={myTheme}>{JSON.stringify(data)}</ThemeProvider>
+      <div className="font-black">{JSON.stringify(data)}</div>
     </React.Fragment>
   );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import client from './apollo';
 import App from './components/App';
+import './main.css'
 
 ReactDOM.render(
   <React.StrictMode>
