cb418b7b627b37de9d3275c25694ea739a3b4cca
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-09-23 18:19:27 -0400

    yarn add react-app-rewired path

---
+++ client/config-overrides.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const { override, addWebpackAlias } = require('customize-cra');
+
+module.exports = override(
+  addWebpackAlias({
+    '@components': path.resolve(__dirname, 'src/components'),
+    '@routes': path.resolve(__dirname, 'src/routes'),
+  })
+);
