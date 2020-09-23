28aa7f9c024c43dd5d7e6bf6fc690161e53b2e5a
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-09-23 19:27:35 -0400

    yarn add dark theme

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { IS_LOGGED_IN } from './AppQueries';
-import { useQuery } from 'react-apollo-hooks';
+// import { IS_LOGGED_IN } from './AppQueries';
+// import { useQuery } from 'react-apollo-hooks';
 import Login from '@routes/Login';
 
 const App = () => {
-  const { data } = useQuery(IS_LOGGED_IN);
+  // const { data } = useQuery(IS_LOGGED_IN);
 
   return (
-    <React.Fragment>
+    <div className="w-screen h-screen bg-primary text-primary">
       <BrowserRouter>
         <Switch>
           <Route path="/login" component={Login} />
         </Switch>
-        <div className="font-black">{JSON.stringify(data)}</div>
       </BrowserRouter>
-    </React.Fragment>
+    </div>
   );
 };
 
---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      backgroundColor: {
+        primary: '#2d3748',
+        secondary: '#283141',
+      },
+      textColor: {
+        accent: '#f7fafc',
+        primary: '#e2e8f0',
+        secondary: '#81e6d9',
+      },
+    },
+  },
+};
