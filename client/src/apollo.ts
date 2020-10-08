a662e3003423382575b2677386f97d69ad51c608
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-10-07 21:45:42 -0400

    add header authorization

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -1,13 +1,26 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const cache = new InMemoryCache();
 const link = createHttpLink({
   uri: `http://localhost:4000/graphql`,
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('V-JWT');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
   cache,
-  link,
+  link: authLink.concat(link),
   name: 'web-video-call-client',
   queryDeduplication: false,
 });
